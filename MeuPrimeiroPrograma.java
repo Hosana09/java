9efdd93e71aaba9d99ca9cdec9994c0764055059
Hosana09 <hosanapires9@gmail.com>
2024-09-11 08:17:18 -0300

    meu primeiro programa

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrimeiroPrograma{
+    public static void main(String[] args){
+        System.out.println("Hello World! Olá, mundo!");
+        System.out.println("O primeiro de muitos!");
+        System.out.println("O primeiro código escrito em " + args[0] + " pela " + args[1] + "!");
+    }
+}
