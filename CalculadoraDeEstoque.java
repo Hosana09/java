1c1ba984c7524d1beebefc8da8941ce476e2efd9
Hosana09 <hosanapires9@gmail.com>
2024-09-11 09:21:27 -0300

    For na classe CalculadoraDeEstoque

--- CalculadoraDeEstoque.java
+++ CalculadoraDeEstoque.java
@@ -2,12 +2,9 @@ public class CalculadoraDeEstoque {
     public static void main(String[] args) {
 
         double soma = 0;
-        int contador = 0;
 
-        while (contador < 35) {
-            double valorDoLivro = 59.90;
-            soma += valorDoLivro;
-            contador ++;
+        for (double contador = 0; contador < 35; contador++) {
+            soma += 59.90;
         }
 
         System.out.println("O valor total em estoque é de " + soma);
