2f071d7f6b8875804b61a20aee5992ee1ea0a52b
Hosana09 <hosanapires9@gmail.com>
2024-09-11 09:14:41 -0300

    While na classe CalculadoraDeEstoque

--- CalculadoraDeEstoque.java
+++ CalculadoraDeEstoque.java
@@ -1,9 +1,14 @@
 public class CalculadoraDeEstoque {
     public static void main(String[] args) {
-        double livroJava8 = 59.90;
-        double livroTDD = 59.90;
 
-        double soma = livroJava8 + livroTDD;
+        double soma = 0;
+        int contador = 0;
+
+        while (contador < 35) {
+            double valorDoLivro = 59.90;
+            soma += valorDoLivro;
+            contador ++;
+        }
 
         System.out.println("O valor total em estoque é de " + soma);
 
