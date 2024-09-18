62a13aa0f046a7e8fefd76414d6ddc9c12d40b34
Hosana09 <hosanapires9@gmail.com>
2024-09-18 20:55:06 -0300

    Criação do primeiro método

--- Livraria/src/Livro.java
+++ Livraria/src/Livro.java
@@ -6,4 +6,14 @@ public class Livro {
     String descricao;
     double valor;
     String isbn;
+
+    void mostrarDetalhes() {
+        String mensagem = "Mostrando detalhes do livro";
+        System.out.println(mensagem);
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("ISBN :" + isbn);
+        System.out.println("--");
+    }
 }
