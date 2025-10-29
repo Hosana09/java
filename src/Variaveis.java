6dfc598b7c62e041575f69fae2db2c6d20410e21
Hosana Azevedo <hosanapires9@gmail.com>
2025-10-29 20:29:34 -0300

    aula1: variáveis

--- src/Variaveis.java
+++ src/Variaveis.java
@@ -1,3 +1,13 @@
-public class variaveis {
+public class Variaveis {
 
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        // Declarar e inicializar é quando você atribui um valor à variável no momento em que a declara
+        int valor;
+        int num1, num2, num3;
+        int num4, num5 = 4, num6 = 7, num8;
+        char caractere;
+        float preco = 34.2f; // 'f' indica que é um float, o valor de ponto flutuante padrão é double
+        String nome;
+    }
 }
