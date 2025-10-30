e119528da6dc560f9e15542c515fb6feb71e914d
Hosana Azevedo <hosanapires9@gmail.com>
2025-10-30 08:38:19 -0300

    olá, mundo divertido

---
+++ src/OlaMundoDivertido.java
@@ -0,0 +1,13 @@
+public class OlaMundoDivertido {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println("Eu sou um programa Java divertido!");
+        System.out.println("Vamos animar o seu dia!");
+
+        // Adicionando um toque de diversão
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Ha");
+        }
+        System.out.println("!");
+    }
+}
