f090250812abca278dd538218b4b91708ed5700d
Hosana Azevedo <hosanapires9@gmail.com>
2025-10-30 18:07:52 -0300

    comenta variáveis para eliminar alerta

--- src/Variaveis.java
+++ src/Variaveis.java
@@ -4,12 +4,12 @@ public class Variaveis {
     public static void main(String[] args) {
         // Declaração de variáveis
         // Declarar e inicializar é quando você atribui um valor à variável no momento em que a declara
-        int valor;
-        int num1, num2, num3;
-        int num4, num5 = 4, num6 = 7, num8;
-        char caractere;
-        float preco = 34.2f; // 'f' indica que é um float, o valor de ponto flutuante padrão é double
-        String nome;
+        // int valor;
+        // int num1, num2, num3;
+        // int num4, num5 = 4, num6 = 7, num8;
+        // char caractere;
+        // float preco = 34.2f; // 'f' indica que é um float, o valor de ponto flutuante padrão é double
+        // String nome;
         int idade = 25;
 
         System.out.println("Eu tenho " + idade + " anos.");
