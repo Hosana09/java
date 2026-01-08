60f2866e5eda14bc46b7eacf336d79f4eea16c75
Hosana Azevedo <hosanapires9@gmail.com>
2026-01-07 21:07:17 -0300

    aula24: biblioteca Math

---
+++ src/BibliotecaMath.java
@@ -0,0 +1,11 @@
+public class BibliotecaMath {
+    public static void main(String[] args) {
+        System.out.println("O valor de PI é: " + Math.PI);
+        System.out.println("O valor arredondado de PI é: " + Math.round(Math.PI));
+        System.out.println("O valor de E é: " + Math.E);
+        System.out.println("O valor absoluto de -10 é: " + Math.abs(-10));
+        System.out.println("O coseno de 45 graus é: " + Math.cos(45));
+        System.out.println("O valor de 2 elevado a 3 é: " + Math.pow(2, 3));
+        System.out.println("A raiz quadrada de 16 é: " + Math.round(Math.sqrt(16)));
+    }
+}
